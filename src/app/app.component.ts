b7895168f985cafa55c35434d5b8c66c5a429ff7
Georgi Demirchev <georgi@demirchev.com>
2021-01-22 19:06:03 +1100

    +decimals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener } from '@angular/core';
+import { flatten } from '@angular/core/src/render3/util';
 import { from, Observable, Subject, timer } from 'rxjs';
 
 @Component({
@@ -21,6 +22,11 @@ export class AppComponent {
 
   fractions: [number, number, number][] = [];
   shuffledFractions: [number, number, number][] = [];
+
+  decs: [number, number, number][] = [];
+  sDecs: [number, number, number][] = [];
+  aDecs: [number, number, number][] = [];
+  asDecs: [number, number, number][] = [];
 
   where = 0;
 
@@ -65,12 +71,37 @@ export class AppComponent {
         this.fractions.push([rand, l1, r1]);
       }
     }
+
+    for (let i = .01; i < 5.00; i += .01) {
+      const i1 = Math.floor(i * 100) / 100;
+      const dgd = this.getlowestfraction(i1);
+      this.decs.push([i1, dgd[0], dgd[1]]);
+    }
+    // console.log(JSON.stringify(this.dec));
+    // for (let i = 5.01; i < 10.00; i += .01) {
+    //   const i1 = Math.floor(i * 100) / 100;
+    //   const dgd = this.getlowestfraction(i1);
+    //   this.aDecs.push([i1, ...dgd]);
+    // }
+    // console.log(JSON.stringify(this.aDecs));
+
+    // for (let l = 2, i = 0; l < 100; l++, i++) {
+    //   for (let r = 2; r < 100; r++) {
+    //     this.decimals.push([l, r]);
+    //   }
+    // }
   }
 
   ngOnInit() {
     this.shuffledQuests = this.shuffle(this.quests);
     this.shuffledPlusMin = this.shuffle(this.plusMin);
     this.shuffledFractions = this.shuffle(this.fractions);
+    this.sDecs = this.shuffle(this.decs);
+    this.asDecs = this.shuffle(this.decs);
+  }
+
+  rightDec(n: number) {
+    return Math.floor(n * 100) / 100;
   }
 
   inc() {
@@ -89,6 +120,37 @@ export class AppComponent {
     return Math.floor(Math.random() * (max - 1) + 1);
   }
 
+  decFl(n: number) {//return new Number(n).toFixed(2);
+    // return Math.floor((n + Number.EPSILON) * 100) / 100
+    return Math.floor(n);
+  }
+  dec(a: [number, number], b: [number, number]) {
+    return this.decFl(a[0] / a[1]) + this.decFl(b[0] / b[1]);
+  }
+
+  getlowestfraction(x0: number): [number, number] {
+    var eps = 1.0E-15;
+    var h, h1, h2, k, k1, k2, a, x;
+
+    x = x0;
+    a = Math.floor(x);
+    h1 = 1;
+    k1 = 0;
+    h = a;
+    k = 1;
+
+    while (x - a > eps * k * k) {
+      x = 1 / (x - a);
+      a = Math.floor(x);
+      h2 = h1; h1 = h;
+      k2 = k1; k1 = k;
+      h = h2 + a * h1;
+      k = k2 + a * k1;
+    }
+
+    return [h, k];
+  }
+
   // floor(n) {
   //   return Math.floor(n);
   // }
@@ -98,5 +160,8 @@ export class AppComponent {
     event.returnValue = confirm('You may loose your progress, leave anyway?');
   }
 
+  coeff(i: number) {
+    return i%2 ? 1 : -1;
+  }
 
 }
